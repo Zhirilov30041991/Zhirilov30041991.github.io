69379feaa3b12f0c776a1b07f83dc0ca82cf50f2
Zheka <57876338+Zhirilov30041991@users.noreply.github.com>
2019-11-28 12:55:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,23 +7,16 @@ menu.onclick = function myFunction(){
 		x.className = "topnav";
 	}
 }
-// Карусель
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:4,
-    navText : ["<img src=\"img/prev-end.png\">","<img src=\"img/prev-next.png\">"],
-    nav:true,
-
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
