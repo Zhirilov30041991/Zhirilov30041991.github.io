c98e5158efc5b5268862023340b986378f62a9c1
Zheka <57876338+Zhirilov30041991@users.noreply.github.com>
2020-01-13 21:31:11 +0200

    Add files via upload

---
+++ index..php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lesson_6</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="header__logo">
+			<img src="images/header.png" alt="men">
+		</div>
+	</header>
+	<section id="contact" class="contact">
+		<div class="main">
+			<div class="pointer">
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+				<div class="square"></div>
+			</div>
+			<h1>Coming Soon</h1>
+			<form action="" method="post">
+				<div>
+					<label for="first_name">Contact Us</label>
+					<input type="text" id="name" name="name" placeholder="Name">
+				</div>
+				<div>
+					<input type="email" id="email" name="email" placeholder="Email">
+				</div>
+				<div>
+					<input type="tel" id="tel" name="tel" placeholder="Telephone" >
+				</div>
+				<div>
+					<textarea name="text" placeholder="Message"></textarea>
+				</div>
+				<button type="submit" name="send" value="send">Send</button>
+			</form>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<p>Copyright ©2013 Prankster, Inc. - All Rights Reserved.</p>
+		</div>
+	</footer>
+	<script type="text/javascript" src="contactform.js"></script>
+	<script  src="js/main.js"></script>	
+</body>
+</html>
