2e855af973fcc2f42654bba22ea4ded0967c3436
Zheka <57876338+Zhirilov30041991@users.noreply.github.com>
2020-01-13 18:54:38 +0200

    Add files via upload

---
+++ img/js/main.js
@@ -0,0 +1,11 @@
+$(function (){
+	$('.offer-content a').click(function(){
+		$('.triangle').css('width','300px');
+	})
+	$('.phone').click(function(){
+		$('.offer-content h1').css('font-size','10px');
+	})
+		$('.menu li a').click(function(){
+		$('.this').toggleClass('hover');
+	})
+})
