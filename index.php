835cfdbc2f5d0498f9e693f9e34cd9983d36cf1a
Zheka <57876338+Zhirilov30041991@users.noreply.github.com>
2020-01-13 18:38:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" lang="en">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="contact.php">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="fonts/fonts.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome-free-5.11.2-web/css/all.min.css">	
+</head>
+<body>
+	<div id="header" class="header" >
+		<div class="container">
+				<div class="menu">
+					<div class="logo">
+						<img src="img/Logo.png" alt="logo">
+					</div>
+						<ul class="nav">
+							<li><a href="#" class="nav-link">Home</a></li>
+							<li><a href="#" class="nav-link">Pages</a></li>
+							<li><a href="#" class="nav-link">Features</a></li>
+							<li><a href="#" class="nav-link">Extensions</a></li>
+							<li><a href="#" class="nav-link">Tutorials</a></li>
+							<li><a href="#" class="nav-link">Contact us</a></li>
+						</ul>
+				</div>
+				<div class="responsiv-menu">
+					<ul class="nav__responsiv">
+						<li><a href="#" class="nav-link">Home</a></li>
+						<li><a href="#" class="nav-link">Pages</a></li>
+						<li><a href="#" class="nav-link">Features</a></li>
+						<li><a href="#" class="nav-link">Extensions</a></li>
+						<li><a href="#" class="nav-link">Tutorials</a></li>
+						<li><a href="#" class="nav-link">Contact us</a></li>
+					</ul>
+				</div>
+				
+			<div class="offer">
+				<h1>Creatives</h1>
+				<p class="offer-text">Power by PSDfreebies.com</p>
+			</div>
+			<div class="main-arrow">
+				<p>Scroll Down</p>
+				<a class="wq" href="#footer"><img src="img/scrol-3.png" alt="circle"></a>
+				
+			</div>
+		</div>
+	</div>
+
+	<section id="about-agency" class="about-agency">
+		<div class="container">
+			<div class="big-latter">
+				W
+			</div>
+			<div class="small-latter">
+				<h2>We Are An Awesome Agency</h2>
+			</div>
+			<div class="services-clearfix">
+				<div class="services-item">
+					<div class="feature-one">
+						<img src="img/icon-1.png" alt="fearure" class="feature-1">
+						<img src="img/Ellipse-1.png" class="Ellipse" alt="Ellipse">
+					</div>
+					<h3>Feature one</h3>
+					<p>Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus</p>
+				</div>
+				<div class="services-item">
+					<div class="feature-two">
+						<img src="img/icon-1.png" alt="fearure" class="feature-1">
+						<img src="img/Ellipse-1.png" class="Ellipse" alt="Ellipse">
+					</div>
+					<h3>Feature two</h3>
+					<p>Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus</p>
+				</div>
+				<div class="services-item">
+					<div class="feature-three">
+						<img src="img/icon-3.png" alt="fearure" class="feature-1">
+						<img src="img/Ellipse-1.png" class="Ellipse" alt="Ellipse">
+					</div>
+					<h3>Feature three</h3>
+					<p>Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus</p>
+				</div>
+				<div class="services-item">
+					<div class="feature-four">
+						<img src="img/icon-4.png" alt="fearure" class="feature-1">
+						<img src="img/Ellipse-1.png" class="Ellipse" alt="Ellipse">
+					</div>
+					<h3>Feature four</h3>
+					<p>Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus</p>
+				</div>
+				</div>
+				
+			</div>
+	</section>
+		
+		<section id="news-area" class="news-area">
+			<div class="news-area-apple">
+				<div class="news-area-text">
+				<h5><span>Jan 4, 2016</span> In Culture</h5>
+				<h3>Akita Inu Dog</h3>
+				<p>
+					Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+					<br>
+					<br>
+					Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur 
+				</p>
+			</div>
+			<div class="news-area-pictere">
+			</div>
+			</div>
+				<div class="news-area-look">
+					<div class="news-area-text">
+					<h5><span>Jan 4, 2016</span> In Culture</h5>
+					<h3>Akita Inu Dog</h3>
+					<p>
+						Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+						<br>
+						<br>
+						Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur 
+					</p>
+				</div>
+				<div class="news-area-pictere-two">
+				</div>
+			</div>	
+		</section>
+
+		<section id="area" class="area">
+			<div class="container">
+				<div class="big-latter">
+					<p class="area-big-latter">B</p>
+				</div>
+				<div class="area-small-latter">
+					<h2>Let's blog</h2>
+				</div>
+				<div class="area-big-text">
+					<p>We love clean design and advanced digital solutions.</p>
+				</div>
+
+				<div class="summer-clearfix">
+					<div class="fresh-one">
+						<img src="img/Blog-1.png" alt="explanation">
+						<h5>Fresh Summer Campaign</h5>
+						<p>
+							Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+						</p>
+					</div>
+					<div class="fresh-two">
+						<img src="img/Blog-2.png" alt="analysis">
+						<h5>Fresh Summer Campaign</h5>
+						<p>
+							Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+						</p>
+					</div>
+					<div class="fresh-three">
+						<img src="img/Blog-3.png" alt="write">
+						<h5>Fresh Summer Campaign</h5>
+						<p>
+							Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+						</p>
+					</div>
+					<div class="fresh-four">
+						<img src="img/Blog-1.png" alt="explanation">
+						<h5>Fresh Summer Campaign</h5>
+						<p>
+							Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+						</p>
+					</div>
+					
+				</div>
+			</div>	
+		</section>
+		<div  class="action-area" id="action-area">
+			<div class="container">
+				<div class="area">
+						<div class="quick-action-one">
+							<img src="img/Write-2.png" alt="pen">
+							<p class="quick-text">
+								Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+							</p>
+						</div>
+						<div class="quick-action-one">
+							<img src="img/Write-2.png" alt="pen">
+							<p class="quick-text">
+								Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+							</p>
+						</div>
+				</div>
+			</div>
+		</div>
+		<section id="key-fuature" class="key-fuature">
+			<div class="container">
+				<div class="qw">
+						<div class="key-text">
+					<h3>Akita Inu Dog</h3>
+					<p>
+					Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+					<div class="desing-text">
+						<p class="desing-text-one">Fully Responsive Design</p>
+						<p class="desing-text-two">High Quality Code</p>
+						<p class="desing-text-three">Different Headers & Layout</p>
+					</div>	
+				</div>
+				<div class="key-phone">
+						<img src="img/phone.png" alt="phone">
+				</div>
+				</div>
+			</div>
+		</section>
+		<section id="team" class="team">
+			<div class="container">
+				<div class="team-text">
+					<div class="big-latter">
+					<p class="area-big-latter">T</p>
+				</div>
+				<div class="area-small-latter">
+					<h2>Our Team</h2>
+				</div>
+				<div class="area-big-text">
+					<p> sales long tail influencer pitch release niche market.</p>
+				</div>
+				</div>
+				<div class="team-clearfix-w">
+					<div class="team-one">
+						<img src="img/people-1.png" alt="John Snow">
+						<h5>JOHN SNOW<br><span>CEO</span></h5>
+					</div>
+					<div class="team-one">
+						<img src="img/people-2.png" alt="Lady Sansa">
+						<h5>LADY SANSA<br><span>WebDesigner</span></h5>
+					</div>
+					<div class="team-one">
+						<img src="img/people-3.png" alt="Zheka">
+						<h5>Евгений Николаевич<br><span>Photographer</span></h5>
+					</div>
+					<div class="team-one">
+						<img src="img/people-4.png" alt="Lady Sansa">
+						<h5>LADY SANSA<br><span>Photographer</span></h5>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="contact-area" class="contact-area">
+			<div class="container">
+					<div class="big-latter">
+					C
+					</div>
+					<div class="qw-small-latter">
+							<h2>We’d love to hear about your project.</h2>
+					</div>
+
+					<form action="#" method="POST" class="form">
+						<input type="text" name="text" class="input" placeholder="Name" required>
+						<input type="text" name="text" class="input" placeholder="Email" required>
+						<input type="text" name="text" class="input" placeholder="Phone" required>
+					</form>
+					<form action="#" method="POST" class="form-one">
+						<input type="text" name="text" class="massenger" placeholder="Message" required>
+					</form>
+					<div class="btn">
+						<button type="submit">Submit</button>
+					</div>
+			</div>
+		</section>
+		<div id="map" class="map"></div>
+		<footer id="footer" class="footer">
+			<div class="container">
+				<div class="footer-text">
+					<p>© 2016 PSDfreebies.com – All Right Reserved</p>
+					<img src="img/footer.png" alt="footer">
+				</div>
+			</div>
+		</footer>
+
+	<script>
+		function initMap() {
+  				var uluru = {lat: 50.0026949, lng: 36.3048336};
+  				var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  		var marker = new google.maps.Marker({position: uluru, map: map});
+		}
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key&callback=initMap">
+    </script>
+    <script type="text/javascript" src="contactform.js"></script>
+    <script type="text/javascript" src="lib/jQuery/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,33 +1,8 @@
-menu.onclick = function myFunction(){
-	var x = document.getElementById('myTopnav');
-
-	if (x.className === "topnav") {
-		x.className += " responsive"
-	} else {
-		x.className = "topnav";
-	}
-}
-var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+// Якорь с прогруткой
+$('.main-arrow a').on('click', function (e) {
+        e.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1500, 'linear');
     });
-
-var vid = document.getElementById("myVideo"); 
-
-function playVid() { 
-  vid.play(); 
-} 
-
-function pauseVid() { 
-  vid.pause(); 
-}
-
